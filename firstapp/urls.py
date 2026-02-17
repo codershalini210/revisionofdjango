5fefca0013f126e56b8d8123cddf7310bc922b3e
shalini <s.rathore43@gmail.com>
2026-02-17 15:24:44 +0530

    khukj

--- firstapp/urls.py
+++ firstapp/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 from . import views
 urlpatterns=[
-    path('',views.home,name='home')
+    path('',views.home,name='home'),
+    path('cuisinelist',views.Cuisine_list,name='Cuisine_list'),
+    path('cuisineDetails/<int:pk>/',views.Cuisine_detail,name='Cuisine_Cuisine_detaillist'),
+
 ]
--- firstapp/views.py
+++ firstapp/views.py
@@ -1,5 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_list_or_404
 from django.http import HttpResponse
+from .models import Cuisine
 # Create your views here.
 def home(request):
     return HttpResponse("welcome to home of first app ")
+
+def Cuisine_list(request):
+    cuisines = Cuisine.objects.all()
+    context = {'cuisines':cuisines}
+    return render(request,"firstapp/cuisine_list.html",context)
+def Cuisine_detail(request,pk):
+    cuisine_selected = get_list_or_404(Cuisine,pk=pk,status='published')
+    context = {'cuisine': cuisine_selected}
+    print(cuisine_selected)
+    return render(request,'firstapp/cuisine_details.html',context)
