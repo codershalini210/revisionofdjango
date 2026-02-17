9d7e62976bfde30483da9c3f6ab5c1cb8b1a1255
shalini <s.rathore43@gmail.com>
2026-02-17 16:56:40 +0530

    try upto list and details view

--- firstapp/views.py
+++ firstapp/views.py
@@ -1,14 +1,25 @@
 from django.shortcuts import render,get_object_or_404
 from django.http import HttpResponse
 from .models import Cuisine
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 # Create your views here.
 def home(request):
     return HttpResponse("welcome to home of first app ")
 
 def Cuisine_list(request):
-    cuisines = Cuisine.objects.all()
-    context = {'cuisines':cuisines}
-    return render(request,"firstapp/cuisine_list.html",context)
+    objlist = Cuisine.objects.all()
+    paginator = Paginator(objlist,2)  # 2 items in each page
+    page = request.GET.get('page',1)
+    print(page)
+    try:
+        cuisines = paginator.page(page)
+     
+    except PageNotAnInteger:
+        cuisines= paginator.page(1)
+    except EmptyPage:
+        cuisines = paginator.page(paginator.num_pages)
+
+    return render(request,"firstapp/cuisine_list.html",{'page':page,'cuisines':cuisines})
 def Cuisine_detail(request,pk):
     cuisine_selected = get_object_or_404(Cuisine,pk=pk,status='published')
     
