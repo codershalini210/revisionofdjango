39ace37f49890627291f7ec8692b9345163588af
shalini <s.rathore43@gmail.com>
2026-02-18 15:16:20 +0530

    usingforms

--- demoproject/urls.py
+++ demoproject/urls.py
@@ -21,5 +21,6 @@ from django.urls import include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("first/",include('firstapp.urls')),
-    path("library/",include('library.urls'))
+    path("library/",include('library.urls')),
+    path("contact/",include('contact.urls')),
 ]
